d042a3645a09a028f59faa52b6e273f42542a5da
rivazahra <riva.zahra88@gmail.com>
2023-10-07 19:58:11 +0700

    Fix error

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-
-// Fungsi untuk mengimpor dan menyisipkan konten navbar
-function includeNavbar() {
-    fetch('navbar.html')
+    // Fungsi untuk mengimpor dan menyisipkan konten navbar
+    function includeNavbar() {
+        fetch('navbar.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('navbar-container').innerHTML = data;
+            const menuList = document.getElementById('menu-list');
+            const menuIcon = document.getElementById("menu-icon");
+           menuIcon.addEventListener("click", () => {
+            menuList.classList.toggle('hidden')            
+
+           })
+
         });
+    }
+    
+    function includeContact(){
+    fetch('contact.html')
+    .then(response => response.text())
+    .then(data =>{
+        document.getElementById('contact-container').innerHTML = data;
+    } )
 }
 
 // Panggil fungsi includeNavbar untuk mengimpor navbar
 includeNavbar();
+includeContact()
+
